c9eb26509b4977f1d10493674f618408a5b3f733
hsakurai-cq <h.sakurai@caraquri.com>
2017-07-24 10:39:27 +0900

    Login画面作成

---
+++ app/src/main/java/com/hiromisakurai/bookapp/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.hiromisakurai.bookapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+}
