9b3bcb1ff21f7ed506abb7cf416400101a5008e2
hsakurai-cq <h.sakurai@caraquri.com>
2017-07-24 13:55:33 +0900

    ErrorDialogをstaticに修正、MainActivityに画面遷移

--- app/src/main/java/com/hiromisakurai/bookapp/ErrorDialogUtil.java
+++ app/src/main/java/com/hiromisakurai/bookapp/ErrorDialogUtil.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.support.v7.app.AlertDialog;
 
 public class ErrorDialogUtil {
-    public void showDialog(String error, Context context) {
+    public static void showDialog(String error, Context context) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context)
                 .setTitle(R.string.alert_title)
                 .setPositiveButton("OK", null)
--- app/src/main/java/com/hiromisakurai/bookapp/LoginActivity.java
+++ app/src/main/java/com/hiromisakurai/bookapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.hiromisakurai.bookapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -35,12 +36,12 @@ public class LoginActivity extends AppCompatActivity {
                 String password = passwordEditText.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
-                    ErrorDialogUtil emailErrorLog = new ErrorDialogUtil();
-                    emailErrorLog.showDialog("No email, Please enter your email!", LoginActivity.this);
-                }
-                if (TextUtils.isEmpty(password)) {
-                    ErrorDialogUtil passwordErrorLog = new ErrorDialogUtil();
-                    passwordErrorLog.showDialog("No password, Please enter your password!", LoginActivity.this);
+                    ErrorDialogUtil.showDialog("No email, Please enter your email!", LoginActivity.this);
+                }else if (TextUtils.isEmpty(password)) {
+                    ErrorDialogUtil.showDialog("No password, Please enter your password!", LoginActivity.this);
+                } else {
+                    Intent intent = new Intent(getApplication(), MainActivity.class);
+                    startActivity(intent);
                 }
             }
         });
