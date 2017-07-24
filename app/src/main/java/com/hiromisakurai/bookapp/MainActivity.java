bb577c78c970c99c113dbacda59e8f6754518b48
hsakurai-cq <h.sakurai@caraquri.com>
2017-07-24 20:41:45 +0900

    TabLayout, ViewPagerを実装

--- app/src/main/java/com/hiromisakurai/bookapp/MainActivity.java
+++ app/src/main/java/com/hiromisakurai/bookapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.hiromisakurai.bookapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new BookListFragment(), "Book List");
+        viewPagerAdapter.addFragments(new SettingFragment(), "Setting");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
