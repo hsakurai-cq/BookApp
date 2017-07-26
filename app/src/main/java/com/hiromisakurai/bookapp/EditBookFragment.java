42b35a0efcde7c391b502a632bc81c2a83fda2ca
hsakurai-cq <h.sakurai@caraquri.com>
2017-07-26 15:57:17 +0900

    BottomNavigationViewに合わせたToolBarの挙動を修正

--- app/src/main/java/com/hiromisakurai/bookapp/EditBookFragment.java
+++ app/src/main/java/com/hiromisakurai/bookapp/EditBookFragment.java
@@ -1,20 +1,16 @@
 package com.hiromisakurai.bookapp;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 public class EditBookFragment extends Fragment {
-
 
     public EditBookFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,5 +18,4 @@ public class EditBookFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_book, container, false);
     }
-
 }
--- app/src/main/java/com/hiromisakurai/bookapp/MainActivity.java
+++ app/src/main/java/com/hiromisakurai/bookapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +19,26 @@ public class MainActivity extends AppCompatActivity {
         manager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
 
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Button btn = (Button)findViewById(R.id.button_add);
+            TextView title = (TextView)findViewById(R.id.toolbar_main_title);
+
             switch (item.getItemId()) {
                 case R.id.menu_book:
                     BookListFragment bookListFragment = new BookListFragment();
                     switchFragment(bookListFragment);
+                    title.setText(R.string.toolbar_title_list);
+                    btn.setVisibility(View.VISIBLE);
                     return true;
                 case R.id.menu_setting:
                     SettingFragment settingFragment = new SettingFragment();
                     switchFragment(settingFragment);
+                    title.setText(R.string.toolbar_title_setting);
+                    btn.setVisibility(View.GONE);
                     return true;
             }
             return true;
@@ -56,10 +64,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplication(), AddBookActivity.class);
                 startActivity(intent);
             }
-
         });
-
     }
-
-
 }
