6eedb3014c617080dff7e68b370dc036c9de4c1d
hsakurai-cq <h.sakurai@caraquri.com>
2017-07-27 16:24:51 +0900

    アカウント設定 fragment と、作成画面までの intent を実装

--- app/src/main/java/com/hiromisakurai/bookapp/SettingFragment.java
+++ app/src/main/java/com/hiromisakurai/bookapp/SettingFragment.java
@@ -1,11 +1,14 @@
 package com.hiromisakurai.bookapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -24,6 +27,19 @@ public class SettingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button btn = (Button)view.findViewById(R.id.button_configuration);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplication(), AccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
