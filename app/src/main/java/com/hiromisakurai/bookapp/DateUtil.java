6ddcdaf11da3bd71dce49290329fad156f4b1cf0
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-07 18:51:29 +0900

    SimpleDateFormat の Locale を設定

--- app/src/main/java/com/hiromisakurai/bookapp/DateUtil.java
+++ app/src/main/java/com/hiromisakurai/bookapp/DateUtil.java
@@ -6,19 +6,19 @@ import android.util.Log;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateUtil {
 
     public static String changeFormat(String time) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
         try {
             if (TextUtils.isEmpty(time)) {
                 time = "2000-01-01";
                 return time;
             }else {
                 Date date = dateFormat.parse(time);
-                String stringFromDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
-                return stringFromDate;
+                return new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(date);
             }
         } catch (ParseException e) {
             Log.i("Parse error", String.valueOf(e));
