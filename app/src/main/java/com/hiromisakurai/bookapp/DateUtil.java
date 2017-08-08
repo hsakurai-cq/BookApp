7c580c0245d92fdee523cc3a2eee0601cd2c8b4b
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-08 18:02:16 +0900

    DateUtil class を修正

--- app/src/main/java/com/hiromisakurai/bookapp/DateUtil.java
+++ app/src/main/java/com/hiromisakurai/bookapp/DateUtil.java
@@ -1,8 +1,5 @@
 package com.hiromisakurai.bookapp;
 
-import android.text.TextUtils;
-import android.util.Log;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,14 +13,10 @@ public class DateUtil {
     public static String changeFormat(String time) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(originalFormat, Locale.ENGLISH);
         try {
-            if (TextUtils.isEmpty(time)) {
-                time = "2000-01-01";
-                return time;
-            }
             Date date = dateFormat.parse(time);
             return new SimpleDateFormat(displayFormat, Locale.ENGLISH).format(date);
         } catch (ParseException e) {
-            Log.i("Parse error", String.valueOf(e));
+            e.printStackTrace();
         }
         return null;
     }
